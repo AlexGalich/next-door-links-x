b31009fbf05c3e075822e08adf22bad1568b8e42
Alex <random@gmail.com>
2023-10-21 18:05:32 +0200

    fix

--- dev.py
+++ dev.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def create_link(type, code):
+def create_link(property_type, property_code):
 
-    link = f"http://next-door.com.ua/uk/{type}/view/{code}"
+    link = f"http://next-door.com.ua/uk/{property_type}/view/{property_code}"
     return link
 
 print(create_link("flats","SF-2-699-320"))
---
+++ main.py
@@ -0,0 +1,10 @@
+from dev import create_link
+
+def main():
+    create_link(
+        property_type = "flats",
+        property_code= "SF-2-699-320"
+    )
+
+if __name__ == '__main__':
+    main()
